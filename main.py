199e2143ca272729a837f631f2ed55789901d55b
Jan Van Balen <jan.vanbalen@uantwerpen.be>
2021-04-02 17:55:14 +0200

    use a queue

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 import sys
 import random
 import logging
-from typing import Iterable
 from time import time, sleep
+from queue import PriorityQueue
+from typing import Iterable
 from dataclasses import dataclass
 from argparse import ArgumentParser
 
@@ -61,12 +62,14 @@ class Tables(object):
         start_time = time()
         last_received = start_time - self.receive_interval
         last_sent = start_time - self.send_interval
+        queue_off = PriorityQueue()
         while True:
             frame_time = time()
             if frame_time - last_received > self.receive_interval:
-                notes = self.receive_notes()
+                notes = list(self.receive_notes())
                 last_received = frame_time
-            self.send_midi(notes, frame_time - start_time, last_sent - start_time)
+                queue_on = self.notes_to_queue(notes)
+            self.send_midi(queue_on, queue_off)
             last_sent = frame_time
             sleep(self.send_interval)
 
@@ -80,40 +83,50 @@ class Tables(object):
         logging.debug('Parsing...')
         sheet = self.sheet.sheet1.get_all_values()
         for row in sheet[1:]:
-            logging.debug(f'Row: {row}')
             try:
                 note = note_from_dict(dict(zip(sheet[0], row)))
-                logging.info(f'Adding {note}')
-                notes.append(note)
+                logging.debug(f'Adding {note}')
+                yield note
             except (ValueError, TypeError) as e:
                 logging.warning(f'Error caught during parsing: {e}')
                 logging.warning(f'Note could not be parsed: {dict(zip(sheet[0], row))}')
 
-        return notes
+    def notes_to_queue(self, notes: Iterable[Note], eps: float = 0.002) -> PriorityQueue[(float, Note)]:
 
-    def send_midi(self, notes, t_now, t_prev) -> None:
-        """Send out any MIDI onsets or offsets that were due between t_prev and t_now.
-        
-        NOTE: the loop logic is handled by first converting all times to a kind of
-        phase `phi` representing the notes' onset and offset as a number in (0, 1).
-
-        Args:
-        - notes (Iterable[Note]): iterable of Note objects
-        - t_now (float): time of current iteration, since start time 
-        - t_prev (float): time of previous iteration, since start time 
-        """
+        now = time() + eps
+        queue = PriorityQueue()
         for note in notes:
-            phi_now = t_now % note.loop
-            phi_prev = t_prev % note.loop
-            if (time_to_play(note.onset % note.loop, phi_now, phi_prev) and
-                random.random() < note.probability):
-                event = NOTE_ON + note.channel, note.pitch, note.velocity
-                self.midi_out.send(event)
-                logging.debug(f'NOTE_ON for note {note}')
-            if time_to_play((note.onset + note.duration) % note.loop, phi_now, phi_prev):
-                event = NOTE_OFF + note.channel, note.pitch, 0
-                self.midi_out.send(event)
-                logging.debug(f'NOTE_OFF for note {note}')
+            onset = now - now % note.loop + note.onset
+            if onset < now:
+                onset += note.loop
+            queue.put((onset, note))
+
+        return queue
+
+    def send_midi(self, queue_on: PriorityQueue[(float, Note)], queue_off: PriorityQueue[(float, Note)]) -> None:
+
+        while queue_off.queue:  # send out NOTE_OFF events
+            t, note = queue_off.queue[0]
+            if time() > t:
+                logging.debug(f'Note off: {note}')
+                self.midi_out.send((NOTE_OFF + note.channel, note.pitch, 0))
+                queue_off.get()
+            else:
+                break
+
+        while queue_on.queue:  # send out NOTE_ON events
+            t, note = queue_on.queue[0]
+            late = time() - t
+            if late > 0:
+                if random.random() < note.probability:
+                    self.midi_out.send((NOTE_ON + note.channel, note.pitch, note.velocity))
+                    late_warning = f'{late * 1000:.2f} ms late' if late > self.send_interval else ''
+                    logging.debug(f'Note on: {note} ' + late_warning)
+                    queue_off.put((t + note.duration, note))
+                queue_on.get()
+                queue_on.put((t + note.loop, note))
+            else:
+                break
 
 
 def time_to_play(phi_note, phi_now, phi_prev):
